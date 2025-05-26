7d14bc1a73aaeab9f728488008ebad511c3d999a
Shubham Sabale <shubhamsabale8421@gmail.com>
2025-05-26 22:45:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+import GfGWeatherApp from "./GfGWeatherApp.jsx"
+
+
+ReactDOM.render(
+  <>
+      <GfGWeatherApp></GfGWeatherApp>
+       </>,
+  document.getElementById('root')
+
+);
+
+
